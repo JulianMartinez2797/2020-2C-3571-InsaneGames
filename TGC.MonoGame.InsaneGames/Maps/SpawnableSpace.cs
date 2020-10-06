ce270472d38a391be44764a583330869af5d0c06
Matias Szlajen <mszlajen@dblandit.com>
2020-10-06 00:08:43 -0300

    Hotfix to spawn mechanic

--- TGC.MonoGame.InsaneGames/Maps/SpawnableSpace.cs
+++ TGC.MonoGame.InsaneGames/Maps/SpawnableSpace.cs
@@ -16,9 +16,11 @@ namespace TGC.MonoGame.InsaneGames.Maps
         public Vector3 GetSpawnPoint(bool floorLevel)
         {
             var area = Areas[Random.Next(0, Areas.Length)];
+            var size = area.Item1;
+            var center = area.Item2;
             float x, y, z;
             x = (float) Random.NextDouble() * area.Item1.X - area.Item1.X / 2 + area.Item2.X;
-            y = floorLevel ? area.Item2.Y - area.Item1.Y / 2 : 
+            y = floorLevel ? area.Item2.Y - area.Item1.Y / 2  + float.MinValue: 
                 (float) Random.NextDouble() * area.Item1.Y - area.Item1.Y / 2 + area.Item2.Y;
             z = (float) Random.NextDouble() * area.Item1.Z - area.Item1.Z / 2 + area.Item2.Z;
             return new Vector3(x, y, z);
