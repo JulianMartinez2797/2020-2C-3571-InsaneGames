8f7e510f268829585303a9a8ad89ee5c20e949a7
Ian Vigilante <i.vigilante@me.com>
2021-03-11 22:24:08 -0300

    ..

--- TGC.MonoGame.InsaneGames/Entities/Bullets/MissileExplosion.cs
+++ TGC.MonoGame.InsaneGames/Entities/Bullets/MissileExplosion.cs
@@ -12,15 +12,18 @@ namespace TGC.MonoGame.InsaneGames.Entities.Bullets
         protected Vector3 ExplosionSize;
         protected Model Model;
         protected Matrix World;
+        protected Matrix Position;
         protected Matrix Scale;
+        protected float current_scale;
         protected Vector3 SpawnPoint;
 
         protected bool is_initialized = false;
 
         public override void Initialize(TGCGame game)
         {
+            current_scale = 0.1f;
             is_initialized = true;
-            Scale = Matrix.CreateScale(1f);
+            Scale = Matrix.CreateScale(current_scale);
 
             float distInFrontOfCam = 0f;
             float upOrDownOffset = 5f;
@@ -29,16 +32,44 @@ namespace TGC.MonoGame.InsaneGames.Entities.Bullets
             Matrix cameraWorld = Matrix.Invert(MapRepo.CurrentMap.Camera.View);
             cameraWorld.Translation += (cameraWorld.Forward * distInFrontOfCam) + (cameraWorld.Up * upOrDownOffset) + (cameraWorld.Right * leftOrRightOffset);
             cameraWorld = Matrix.CreateTranslation(SpawnPoint);
+            Position = cameraWorld;
             World = Scale * cameraWorld;
-            // World = Matrix.CreateTranslation(SpawnPoint)
             base.Initialize(game);
         }
         public override bool isInitialized()
         {
             return is_initialized;
         }
+
+        private bool explosion_going = true;
+        private bool draw_model = true;
         public override void Update(GameTime gameTime)
         {
+            if (draw_model)
+            {
+                if (explosion_going)
+                    current_scale *= 1.275f;
+                else
+                    current_scale -= 0.1f;
+                
+                if (current_scale > 3.5f)
+                    explosion_going = false;
+                if (!explosion_going && current_scale <= 0.1f)
+                    draw_model = false;
+                World = Matrix.CreateScale(current_scale) * Position;
+            }
+            
+            // if (!explosion_finished && current_scale < 3.5f)
+            // {
+            //     current_scale *= 1.275f;
+            // } else {
+            //     explosion_finished = true;
+            // }
+
+            // if (explosion_finished) {
+            //     current_scale -= 0.1f; 
+            // }
+            
             if (!Collided)
             {
                 // LastPosition = CurrentPosition;
@@ -55,7 +86,7 @@ namespace TGC.MonoGame.InsaneGames.Entities.Bullets
         }
         public override void Draw(GameTime gameTime)
         {
-            if (!Collided)
+            if ( draw_model)
                 Model.Draw(World, MapRepo.CurrentMap.Camera.View, MapRepo.CurrentMap.Camera.Projection);
         }
         public MissileExplosion(float baseDamage, Vector3 speed, Vector3 initialPos, Vector3 hitboxSize, Vector3 explosionSize) : base(baseDamage, speed, initialPos, hitboxSize)
@@ -74,7 +105,7 @@ namespace TGC.MonoGame.InsaneGames.Entities.Bullets
             if(!Collided)
             {
                 Console.WriteLine("Colision de Misil");
-                // Collided = true;
+                Collided = true;
             }
         }
     }
